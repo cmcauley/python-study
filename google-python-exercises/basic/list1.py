d52930d345b1e50ba9deafe7f923861e1ccc0289
Chad McAuley <chad.mcauley@gmail.com>
2017-06-02 15:30:13 -0500

    complete solutions for list1.py

--- google-python-exercises/basic/list1.py
+++ google-python-exercises/basic/list1.py
@@ -21,8 +21,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
+  count = 0
+  for s in words:
+      if len(s) >= 2 and s[0] == s[-1]:
+          count += 1 # +++your code here+++
+  return count
 
 
 # B. front_x
@@ -33,8 +36,14 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
-  return
+  xList = []
+  noxList = []
+  for s in words:
+      if s[0] == 'x':
+          xList.append(s)
+      else:
+          noxList.append(s)
+  return sorted(xList) + sorted(noxList)
 
 
 
@@ -46,8 +55,10 @@ def front_x(words):
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
   # +++your code here+++
-  return
+  return sorted(tuples, key=tupleSortLast)
 
+def tupleSortLast(s):
+    return s[-1]
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
@@ -75,7 +86,7 @@ def main():
   test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
        ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-       
+
   print
   print 'sort_last'
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
